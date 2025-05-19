98c3a28c982962b9dbe0a5902431e2685a2c0b80
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-05-19 21:22:10 +0800

    some fix

--- vite.config.js
+++ vite.config.js
@@ -18,41 +18,43 @@ function generateManifest() {
 }
 
 // https://vite.dev/config/
-export default defineConfig({
-	define: {
-		__APP_INFO__: JSON.stringify({
-			build: new Date().toISOString(),
-			version: pkg.version,
-		}),
-	},
-	plugins: [
-		sassGlobImports(),
-		// vueDevTools(),
-		{
-			name: "logo-to-png",
-			buildStart() {
-				const wait = [];
-				for (const size of [16, 32, 48, 128]) {
-					wait.push(
-						sharp("public/assets/img/logo.svg")
-							.resize(size, size)
-							.png({ compressionLevel: 9 })
-							.toFile(`public/assets/img/logo/${size}.png`),
-					);
-				}
-				return Promise.all(wait);
-			},
+export default defineConfig(({ mode }) => {
+	return {
+		define: {
+			__APP_INFO__: JSON.stringify({
+				build: new Date().toISOString(),
+				version: pkg.version,
+			}),
 		},
-		vue(),
-		webExtension({
-			manifest: generateManifest,
-			watchFilePaths: ["src/content/**/*.*"],
-			skipManifestValidation: true,
-			webExtConfig: {
-				chromiumProfile: fs.realpathSync(__dirname + "/.chrome-profile"),
-				keepProfileChanges: true,
-				startUrl: ["https://www.youtube.com/watch?v=ghWpp_iNkLg"],
+		plugins: [
+			sassGlobImports(),
+			// vueDevTools(),
+			{
+				name: "logo-to-png",
+				buildStart() {
+					const wait = [];
+					for (const size of [16, 32, 48, 128]) {
+						const outputFile = `public/assets/img/logo/${size}.png`;
+						if (mode === "production" || !fs.existsSync(outputFile)) {
+							wait.push(
+								sharp("public/assets/img/logo.svg").resize(size, size).png({ compressionLevel: 9 }).toFile(outputFile),
+							);
+						}
+					}
+					return Promise.allSettled(wait);
+				},
 			},
-		}),
-	],
+			vue(),
+			webExtension({
+				manifest: generateManifest,
+				watchFilePaths: ["src/inject/**/*.*"],
+				skipManifestValidation: true,
+				webExtConfig: {
+					chromiumProfile: fs.realpathSync(__dirname + "/.chrome-profile"),
+					keepProfileChanges: true,
+					startUrl: ["https://www.youtube.com/watch?v=ghWpp_iNkLg"],
+				},
+			}),
+		],
+	};
 });
