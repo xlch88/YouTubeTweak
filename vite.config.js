66fb8b67c642db4392cab812d51a824f0ec408fa
n0099 <n@n0099.net>
2025-05-19 04:39:11 +0000

    使用LF而非CRLF换行符 @ .prettierrc 添加命令`npm run prettier` @ package.json 编译时启用sourcemap（需在 https://developer.chrome.com/docs/devtools/settings/ignore-list 中启用`Content scripts injected by extensions`）并禁用minify @ vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,8 @@ import fs from "node:fs";
 import pkg from "./package.json";
 import manifest from "./manifest.json";
 
-[
-	"public/assets/img/logo",
-	".chrome-profile"
-].forEach(dirPath => {
-	if (!fs.existsSync(dirPath))
-		fs.mkdirSync(dirPath);
+["public/assets/img/logo", ".chrome-profile"].forEach((dirPath) => {
+	if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
 });
 
 // https://vite.dev/config/
@@ -24,19 +20,22 @@ export default defineConfig({
 			version: pkg.version,
 		}),
 	},
+	build: { sourcemap: "inline", minify: false },
 	plugins: [
 		sassGlobImports(),
 		vueDevTools(),
 		vue(),
 		{
 			name: "logo-to-png",
-			buildStart: () => Promise.all(
-				[16, 32, 48, 128].map(size =>
-					sharp("public/assets/img/logo.svg")
-						.resize(size, size)
-						.png({ compressionLevel: 9 })
-						.toFile(`public/assets/img/logo/${size}.png`))
-			),
+			buildStart: () =>
+				Promise.all(
+					[16, 32, 48, 128].map((size) =>
+						sharp("public/assets/img/logo.svg")
+							.resize(size, size)
+							.png({ compressionLevel: 9 })
+							.toFile(`public/assets/img/logo/${size}.png`),
+					),
+				),
 		},
 		webExtension({
 			manifest: () => ({
