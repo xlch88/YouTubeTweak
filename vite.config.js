eb70a70ee849513223cafac3a4a2cf3f9b36f0ae
n0099 <n@n0099.net>
2025-05-19 03:22:59 +0000

    创建所需空目录 内联`generateManifest()` 启用vite插件`vueDevTools` @ vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import webExtension from "vite-plugin-web-extension";
 import vueDevTools from "vite-plugin-vue-devtools";
 import sassGlobImports from "vite-plugin-sass-glob-import";
-
+import sharp from "sharp";
+import fs from "node:fs";
 import pkg from "./package.json";
 import manifest from "./manifest.json";
-import fs from "node:fs";
-import sharp from "sharp";
 
-function generateManifest() {
-	return {
-		version: pkg.version,
-		description: pkg.description,
-		...manifest,
-	};
-}
+[
+	"public/assets/img/logo",
+	".chrome-profile"
+].forEach(dirPath => {
+	if (!fs.existsSync(dirPath))
+		fs.mkdirSync(dirPath);
+});
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,29 +26,28 @@ export default defineConfig({
 	},
 	plugins: [
 		sassGlobImports(),
-		// vueDevTools(),
+		vueDevTools(),
+		vue(),
 		{
 			name: "logo-to-png",
-			buildStart() {
-				const wait = [];
-				for (const size of [16, 32, 48, 128]) {
-					wait.push(
-						sharp("public/assets/img/logo.svg")
-							.resize(size, size)
-							.png({ compressionLevel: 9 })
-							.toFile(`public/assets/img/logo/${size}.png`),
-					);
-				}
-				return Promise.all(wait);
-			},
+			buildStart: () => Promise.all(
+				[16, 32, 48, 128].map(size =>
+					sharp("public/assets/img/logo.svg")
+						.resize(size, size)
+						.png({ compressionLevel: 9 })
+						.toFile(`public/assets/img/logo/${size}.png`))
+			),
 		},
-		vue(),
 		webExtension({
-			manifest: generateManifest,
+			manifest: () => ({
+				version: pkg.version,
+				description: pkg.description,
+				...manifest,
+			}),
 			watchFilePaths: ["src/content/**/*.*"],
 			skipManifestValidation: true,
 			webExtConfig: {
-				chromiumProfile: fs.realpathSync(__dirname + "/.chrome-profile"),
+				chromiumProfile: fs.realpathSync(".chrome-profile"),
 				keepProfileChanges: true,
 				startUrl: ["https://www.youtube.com/watch?v=ghWpp_iNkLg"],
 			},
