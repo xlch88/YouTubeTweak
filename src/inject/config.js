2bd95d1f25a2acbf432c56a3b1144d9ef39755d9
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-05-25 03:33:14 +0800

    memory config

--- src/inject/config.js
+++ src/inject/config.js
@@ -1,21 +1,30 @@
+import DefaultConfig from "../defaultConfig.js";
+import { createLogger } from "../logger.js";
+const logger = createLogger("config");
+
 const STORAGE_KEY = "settings";
 
 export default {
+	config: {},
+	memory: {},
+
 	init() {
 		return new Promise((resolve) => {
-			chrome.storage.sync.get(STORAGE_KEY, (res) => {
-				if (res[STORAGE_KEY]) {
-					this.config = res[STORAGE_KEY];
-				} else {
-					this.config = {};
-				}
+			chrome.storage.sync.get(null, (res) => {
+				this.config = { ...DefaultConfig, ...(res[STORAGE_KEY] ?? {}) };
+				this.memory = res["memory"] ?? {};
 				resolve(this.config);
 			});
 		});
 	},
-	get(key = null, defaultValue = null) {
-		if (key === null) return this.config;
+	get(key = null, defaultValue = null, isMemory = false) {
+		if (key === null) return isMemory ? this.memory : this.config;
 
-		return this.config[key] ?? defaultValue;
+		return this[isMemory ? "memory" : "config"][key] ?? defaultValue;
+	},
+	set(key, value, isMemory = false) {
+		logger.log(`set ${isMemory ? "memory" : "setting"} : ${key} ->`, value);
+		if (key) this[isMemory ? "memory" : "config"][key] = value;
+		chrome.storage.sync.set(isMemory ? { memory: this.memory } : { [STORAGE_KEY]: this.config }, () => {});
 	},
 };
