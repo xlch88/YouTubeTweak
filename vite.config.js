c6754b0404a5460e0e975adf33e5afeb5bb1713d
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-05-25 13:25:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -51,30 +51,37 @@ export default defineConfig(({ mode }) => {
 			{
 				name: "manifest-i18n",
 				buildStart() {
+					if (mode === "production") {
+						fs.rmSync("./public/_locales", { recursive: true, force: true });
+					}
 					for (const file of fs.readdirSync("src/assets/i18n").filter((file) => file.endsWith(".json"))) {
 						const name = file.replace(/\.json$/, "");
 						const dir = `./public/_locales/${name.replace("-", "_")}`;
+						const dir2 = `./public/_locales/${name.split("-")[0]}`;
 						const localeData = JSON.parse(fs.readFileSync(`src/assets/i18n/${file}`, "utf-8"));
 
 						const messagesJsonFile = `${dir}/messages.json`;
+						const messagesJsonFile2 = `${dir2}/messages.json`;
+						const messagesContent = JSON.stringify(
+							{
+								manifest_name: {
+									message: localeData.manifest.name,
+								},
+								manifest_description: {
+									message: localeData.manifest.description,
+								},
+							},
+							null,
+							2,
+						);
 
 						if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+						if (!fs.existsSync(dir2)) fs.mkdirSync(dir2, { recursive: true });
 						if (mode === "production" || !fs.existsSync(messagesJsonFile)) {
-							fs.writeFileSync(
-								messagesJsonFile,
-								JSON.stringify(
-									{
-										manifest_name: {
-											message: localeData.manifest.name,
-										},
-										manifest_description: {
-											message: localeData.manifest.description,
-										},
-									},
-									null,
-									2,
-								),
-							);
+							fs.writeFileSync(messagesJsonFile, messagesContent);
+						}
+						if (mode === "production" || !fs.existsSync(messagesJsonFile2)) {
+							fs.writeFileSync(messagesJsonFile2, messagesContent);
 						}
 					}
 				},
