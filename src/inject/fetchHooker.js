79898d7362f22be70ed50f11ae03d836fb45bcaa
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-05-27 15:16:37 +0800

    Create fetchHooker.js

---
+++ src/inject/fetchHooker.js
@@ -0,0 +1,38 @@
+export default {
+	hooks: {},
+
+	init() {
+		const originalFetch = window.fetch;
+		window.fetch = async (...args) => {
+			const url = args[0]?.url || args[0];
+
+			const matchedHooks = Object.values(this.hooks).filter((v) => url.includes(v.match));
+			if (!matchedHooks || typeof url !== "string") return originalFetch.apply(window, args);
+
+			const response = await originalFetch.apply(window, args);
+			const responseClone = response.clone();
+
+			if (matchedHooks.filter((v) => v.mutator).length <= 0) {
+				response
+					.clone()
+					.json()
+					.then((result) => {
+						matchedHooks.forEach((hook) => hook.handler(result, url, responseClone));
+					});
+
+				return response;
+			} else {
+				let data = await response.clone().json();
+				for (const hook of matchedHooks) {
+					data = hook.handler(data, url, responseClone);
+				}
+
+				return new Response(JSON.stringify(data), {
+					status: response.status,
+					statusText: response.statusText,
+					headers: new Headers(response.headers),
+				});
+			}
+		};
+	},
+};
