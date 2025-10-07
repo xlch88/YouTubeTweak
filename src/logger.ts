f8ef6ee8ccec40e55150562e9c1a186b547ca8b6
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-10-07 12:30:02 +0800

    console.error -> console.log

--- src/logger.ts
+++ src/logger.ts
@@ -21,10 +21,11 @@ const colorMap = {
  * @property {Function} error
  */
 export function createLogger(name = "main") {
-	const logger: Record<"log" | "warn" | "debug" | "info" | "error", (...args: any[]) => void> = {} as any;
-	(["log", "warn", "debug", "info", "error"] as const).forEach((type) => {
-		const bg = colorMap[type] || "#7f8c8d";
-		logger[type] = console[type].bind(
+	const logger: Record<keyof typeof colorMap, (...args: any[]) => void> = {} as any;
+	Object.entries(colorMap).forEach(([type, bg]) => {
+		logger[type as keyof typeof colorMap] = (
+			console[isDEV || ["info", "log", "debug"].includes(type) ? (type as keyof typeof console) : "log"] as (...args: any[]) => void
+		).bind(
 			console,
 			`%cYTTweak%c${name}`,
 			`font-family: sans-serif; background: ${bg}; color: white; border-radius: 4px 0 0 4px; padding: 2px 6px; font-weight: bold; font-size: 10px; line-height: 10px;`,
