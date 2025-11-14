9fd26a624003ba7be8e2a2b4af7d1e518501d17f
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-11-14 16:03:28 +0800

    Update chrome-crx3.ts

--- build/chrome-crx3.ts
+++ build/chrome-crx3.ts
@@ -1,11 +1,12 @@
 // @ts-ignore
 import crx3 from "crx3";
+import pkg from "../package.json";
 
 console.log(process.cwd());
 
 await crx3([`${process.cwd()}/.output/chrome-mv3/manifest.json`], {
 	keyPath: `${process.cwd()}/.secret/chrome-web-store.pem`,
-	crxPath: `${process.cwd()}/.output/chrome-mv3.crx`,
+	crxPath: `${process.cwd()}/.output/${pkg.name}-${pkg.version}-chrome.crx`,
 })
 	.then(() => console.log("done"))
 	.catch(console.error);
